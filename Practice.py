2f114a37e0ed3a36b5b609e06688020ac2b62042
Brian <bc06255@georgiasouthern.edu>
2019-01-04 08:48:36 -0500

    More Calculations

--- Practice.py
+++ Practice.py
@@ -8,46 +8,73 @@ class GUI(Frame):
     def __init__(self, master=None):
         Frame.__init__(self, master)
 
+        s = Frame(self, bg="gray")
+        s.grid()
 
-        self.grid()
+        monthFrame = Frame(master, bg="gray80")
+        monthFrame.grid(row=0, column=0, columnspan=5, sticky="ew")
 
-        mLabel = Label(master, text="Select Month", justify=RIGHT, font=("Arial", 20))
-        mLabel.grid(row=1, column=0)
+        mLabel = Label(monthFrame, text="Select Month", justify=RIGHT, font=("Arial", 20), bg="gray80")
+        mLabel.grid(row=0, column=1, padx=(10,0), sticky=E)
 
-        rLabel = Label(master, text="Rent", justify=RIGHT, font=("Arial", 20))
-        rLabel.grid(row=2, column=0, sticky=E)
+        rLabel = Label(monthFrame, text="Rent", justify=RIGHT, font=("Arial", 20), bg="gray80")
+        rLabel.grid(row=1, column=1, sticky=E)
 
-        iLabel = Label(master, text="Income", justify=RIGHT, font=("Arial", 20))
-        iLabel.grid(row=3, column=0, sticky=E)
+        iLabel = Label(monthFrame, text="Income", justify=RIGHT, font=("Arial", 20), bg="gray80")
+        iLabel.grid(row=2, column=1, sticky=E)
 
-        rDollar = Label(master, text="$", font=("Arial", 20))
-        rDollar.grid(row=2, column=1, sticky=E)
+        rDollar = Label(monthFrame, text="$", font=("Arial", 20), bg="gray80")
+        rDollar.grid(row=1, column=2, sticky=E)
 
-        iDollar = Label(master, text="$", font=("Arial", 20))
-        iDollar.grid(row=3, column=1, sticky=E)
+        iDollar = Label(monthFrame, text="$", font=("Arial", 20), bg="gray80")
+        iDollar.grid(row=2, column=2, sticky=E)
 
-        self.month = ttk.Combobox(master,
+        coDollar = Label(monthFrame, text="$", font=("Arial", 20), bg="gray80")
+        coDollar.grid(row=3, column=2, sticky=E)
+
+        coText = Text(monthFrame, font=("Arial", 16), width=8, height=1)
+        coText.grid(row=3, column=3, sticky=W + E, padx=(0, 10))
+
+        cDollar = Label(monthFrame, text="$", font=("Arial", 20), bg="gray80")
+        cDollar.grid(row=4, column=2, sticky=E)
+
+        cText = Text(monthFrame, font=("Arial", 16), width=8, height=1)
+        cText.grid(row=4, column=3, sticky=W + E, padx=(0, 10))
+
+        pDollar = Label(monthFrame, text="$", font=("Arial", 20), bg="gray80")
+        pDollar.grid(row=5, column=2, sticky=E)
+
+        pText = Text(monthFrame, font=("Arial", 16), width=8, height=1)
+        pText.grid(row=5, column=3, sticky=W + E, padx=(0, 10))
+
+        self.month = ttk.Combobox(monthFrame,
                                   values=["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
                                   font=("Arial", 16), width=12, justify=RIGHT)
-        self.month.grid(row=1, column=1, columnspan=2)
+        self.month.grid(row=0, column=2, columnspan=2, padx=(0,10))
 
-        self.rent = Text(master, font=("Arial", 16), width=8, height=1)
-        self.rent.grid(row=2, column=2, sticky=W + E)
+        self.rent = Text(monthFrame, font=("Arial", 16), width=8, height=1)
+        self.rent.grid(row=1, column=3, sticky=W + E, padx=(0,10))
 
-        self.income = Text(master, font=("Arial", 16), width=8, height=1)
-        self.income.grid(row=3, column=2, sticky=W + E)
+        self.income = Text(monthFrame, font=("Arial", 16), width=8, height=1)
+        self.income.grid(row=2, column=3, sticky=W + E, padx=(0,10))
 
-        remainder = Label(master, text="Remainder", justify=CENTER, font=("Arial", 20))
-        remainder.grid(columnspan=3, row=4)
+        remainder = Label(monthFrame, text="Remainder", justify=CENTER, font=("Arial", 20), bg="gray80")
+        remainder.grid(columnspan=5, row=6)
 
-        remDollar = Label(master, text="$", font=("Arial", 20))
-        remDollar.grid(row=4)
+        self.remainderText = Text(monthFrame, font=("Arial", 22), width=14, height=1)
+        self.remainderText.grid(columnspan=3, row=7, column=1)
 
-        self.remainderText = Text(master, font=("Arial", 22), width=14, height=1)
-        self.remainderText.grid(columnspan=3, row=5)
+        calculate = Button(monthFrame, text="Calculate", command=self.calcClick, font=("Arial", 20))
+        calculate.grid(columnspan=3, row=8, column=1, pady=10)
 
-        calculate = Button(master, text="Calculate", command=self.calcClick, font=("Arial", 20))
-        calculate.grid(columnspan=3, row=6)
+        oneLabel = Label(monthFrame, text="Capital One Payment", font=("Arial",20), bg="gray80")
+        oneLabel.grid(row=3, column=1)
+
+        chaseLabel = Label(monthFrame, text="Chase Payment", font=("Arial", 20), bg="gray80")
+        chaseLabel.grid(row=4, column=1, sticky=E)
+
+        phoneLabel = Label(monthFrame, text="Phone Payment", font=("Arial", 20), bg="gray80")
+        phoneLabel.grid(row=5, column=1, sticky=E)
 
         menubar = Menu(root)
 
@@ -81,7 +108,7 @@ class GUI(Frame):
         self.r = float(self.rent.get("1.0", END))
 
         self.remainderText.delete("1.0", END)
-        self.remainderText.insert(END, round(self.i-self.r,2))
+        self.remainderText.insert(END, "$" + str(round(self.i-self.r,2)))
 
     def openClick(self):
 
@@ -101,16 +128,12 @@ class GUI(Frame):
 
         openFile = filedialog.askopenfile()
 
-        title = openFile.name
-        title = title[23:]
-        title = title.replace("_Budget.txt", "")
-
         lines = openFile.readlines()
 
         oMonth = lines[0]
-        oRent = lines[1].replace("Rent:\t\t$", "")
-        oIncome = lines[2].replace("Income:\t\t$", "")
-        oRemainder = lines[4].replace("Remainder:\t$", "")
+        oRent = lines[2].replace("Rent:\t\t$", "")
+        oIncome = lines[3].replace("Income:\t\t$", "")
+        oRemainder = lines[5].replace("Remainder:\t", "")
 
         self.month.insert(END, oMonth)
         self.rent.insert(END, oRent)
@@ -122,17 +145,22 @@ class GUI(Frame):
             print("No Month")
 
         else:
-            m = "C:\\Users\\brian\\Budgets\\" + self.month.get() + "_Budget.txt"
+
+            month = self.month.get()
+            month = month.replace("\n", "")
+
+            m = "C:\\Users\\brian\\Budgets\\" + month + "_Budget.txt"
             f = open(m, "w+")
             fileRent = "Rent:\t\t$" + str(float(self.rent.get("1.0", "8.0")))
             fileIncome = "Income:\t\t$" + str(float(self.income.get("1.0", "8.0")))
-            month = self.month.get()
+
 
             f.write(month + "\n")
+            f.write("-------------------------\n")
             f.write(fileRent + "\n")
             f.write(fileIncome + "\n")
             f.write("-------------------------\n")
-            f.write("Remainder:\t$" + self.remainderText.get("1.0", END))
+            f.write("Remainder:\t" + self.remainderText.get("1.0", END))
 
     def saveasClick(self):
         if self.month.get() == "":
@@ -144,13 +172,13 @@ class GUI(Frame):
             saveas = filedialog.asksaveasfile(mode="w", defaultextension=".txt", title="Testingdefault='.txt", filetypes=my_filetypes)
 
             fileMonth = str(self.month.get())
-            fileRent = "Rent:\t\t$" + str(self.r)
-            fileIncome = "Income:\t\t$" + str(self.i)
-            fileRemainder = "Remainder:\t$" + self.remainderText.get("1.0", END)
+            fileRent = "Rent:\t\t$" + str(float(self.rent.get("1.0", "8.0")))
+            fileIncome = "Income:\t\t$" + str(float(self.income.get("1.0", "8.0")))
+            fileRemainder = "Remainder:\t" + self.remainderText.get("1.0", END)
 
-            self.saved = fileMonth + "\n" + fileRent +"\n" + fileIncome + "\n" + "-------------------------\n" + fileRemainder
+            saved = fileMonth + "-------------------------\n" + fileRent +"\n" + fileIncome + "\n" + "-------------------------\n" + fileRemainder
 
-            saveas.write(self.saved)
+            saveas.write(saved)
             saveas.close()
 
 
